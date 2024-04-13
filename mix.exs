4471bef36941f728e4a42472631ea7ca7ddd630d
Catalin Mititiuc <webdevcat@proton.me>
2024-04-12 20:53:43 -0700

    Avoid compiling the dev server in :prod and starting it in :test

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Serval.MixProject do
       app: :serval,
       version: "0.1.0",
       elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -13,15 +14,21 @@ defmodule Serval.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
       env: [output_directory: "/public"],
-      mod: {Serval.Application, []}
+      extra_applications: [:logger],
+      mod: mod(Mix.env())
     ]
   end
 
+  defp elixirc_paths(:prod), do: ["lib/mix/tasks"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0", only: :dev},
+      {:plug_cowboy, "~> 2.0", only: [:dev, :test]}
     ]
   end
+
+  defp mod(:dev), do: {Serval.Application, []}
+  defp mod(_), do: []
 end
